86590e056437c27ea2ea6dae571d91ce89368dac
boognuel <boognuel@gmail.com>
2026-09-06 18:39:48 -0500

    Implementación función 'Crear Producto'

--- src/app/components/crearproductocomponent/crearproductocomponent.ts
+++ src/app/components/crearproductocomponent/crearproductocomponent.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FakeStoreService } from '../../services/fake-store/fake-store.service';
 
 @Component({
   selector: 'app-crearproductocomponent',
@@ -16,12 +17,24 @@ export class Crearproductocomponent {
     imagen: ''
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private fakeStoreService: FakeStoreService) {}
 
   onSubmit() {
-    console.log('Producto a crear:', this.producto);
-    // TODO: Guardar en DataService / JSON
-    alert('Producto creado exitosamente (Simulación)');
-    this.router.navigate(['/listar-producto']);
+    this.fakeStoreService.createProduct({
+      title: this.producto.nombre,
+      price: Number(this.producto.precio),
+      category: this.producto.categoria,
+      description: this.producto.descripcion,
+      image: this.producto.imagen,
+    }).subscribe({
+      next: () => {
+        alert('Producto creado exitosamente');
+        this.router.navigate(['/listar-producto']);
+      },
+      error: (err) => {
+        console.error('Error al crear producto', err);
+        alert('No se pudo crear el producto.');
+      },
+    });
   }
 }
